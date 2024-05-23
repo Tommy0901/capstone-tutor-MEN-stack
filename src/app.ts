db3ec4733785514396efd5e8f96bf6cb74f6f7f9
Tommy0901 <ak710180@gmail.com>
2024-05-23 15:13:05 +0800

    feat: create OpenAPI 3.1 file and init Swagger UI

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express from 'express'
 import cors from 'cors'
+import swaggerUi from 'swagger-ui-express'
+import swaggerDocument from './docs'
 
 import { router } from './routes/router'
 import { apiErrorHandler } from './middlewares/error-handler'
@@ -8,6 +10,8 @@ const app = express()
 const port = process.env.PORT ?? 3000
 
 app.use(cors(), express.json())
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 for (const route of router) {
   app.use(route.getRouter())
---
+++ src/docs/index.ts
@@ -0,0 +1,11 @@
+const openapi = '3.1.0'
+
+const info = {
+  version: '1.0.0',
+  title: 'Tutor.docs'
+}
+
+export default {
+  openapi,
+  info
+}
